c079d9321646bf4fd65b684fcd130cc4833c2e43
Campbell Barton <ideasman42@gmail.com>
2024-02-04 23:17:05 +1100

    Correct URL

--- scroll-on-jump.el
+++ scroll-on-jump.el
@@ -5,7 +5,7 @@
 
 ;; Author: Campbell Barton <ideasman42@gmail.com>
 
-;; URL: https://codeberg.com/ideasman42/emacs-scroll-on-jump
+;; URL: https://codeberg.org/ideasman42/emacs-scroll-on-jump
 ;; Version: 0.2
 ;; Package-Requires: ((emacs "26.2"))
 
